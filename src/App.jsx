d1f421b18d79462569e4918a0443029d3208f179
Simmo-code <simmo.justin@gmail.com>
2025-12-26 15:08:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import BUILTIN_PROFILES from "./wingProfiles.json";
  *   delta     = after - nominal
  */
 
-const APP_VERSION = "0.3.1";
+const APP_VERSION = "0.3.1a";
 
 /* ------------------------- Helpers ------------------------- */
 
