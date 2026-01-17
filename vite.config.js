c3f08f5d6a360aae6a10992f8b3a04b32111b427
Simmo-code <simmo.justin@gmail.com>
2026-01-17 22:58:17 +0000

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
+  plugins: [react()],
   base: "/Paraglider-trim-tuning/",
+  build: {
+    target: "es2018"
+  }
 });
